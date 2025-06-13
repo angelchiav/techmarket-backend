2f186f5256a9eff8527dbd9358c130a269523426
angelchiav <achiavicuna@gmail.com>
2025-06-13 17:02:57 -0400

    Principal structure of the project

--- products/models.py
+++ products/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=150, null=False)
+    description = models.TextField(null=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2, null = False)
+    in_stock = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.name
+    
+    
--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-class User(models.Model):
-    name = models.CharField(max_length=100)
-    phone = models.CharField(max_length=20)
-    email = models.EmailField(primary_key=True)
+class User(AbstractUser):
+    phone = models.CharField(max_length=20, null=True)
+
+    def __str__(self):
+        return self.name
